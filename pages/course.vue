e732229f36678cd2695117e1ff2c5a75359ea91d
beingmomen <beingmomen@demo.com>
2023-03-02 19:15:49 +0200

    first commit

---
+++ pages/course.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex items-center flex-col p-10 bg-gray-100 h-screen">
+    <h2 class="font-black text-3xl mb-6">Nuxt</h2>
+    <div class="flex w-full justify-center flex-row flex-grow gap-6">
+      <div
+        class="basis-1/4 h-screen bg-white p-4 flex justify-center rounded-md"
+      >
+        <h3 class="">Chapter</h3>
+      </div>
+      <div class="basis-3/4 h-screen bg-red-100 p-4 rounded-md">
+        <NuxtPage />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ pages/course/lesson.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>Lesson</h2>
+    <p>This is a lesson</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+</style>
