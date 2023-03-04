dd552f34f8f4a3b5f8e5350aeb782f21894e2317
beingmomen <beingmomen@demo.com>
2023-03-04 10:11:55 +0200

    push

---
+++ components/VideoPlayer.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- src="https://player.vimeo.com/video/{video_id}?h={hash_parameter}" -->
+  <iframe
+    width="560"
+    height="315"
+    :src="`https://player.vimeo.com/video/${props.videoId}`"
+    frameborder="0"
+    title="Video Player"
+    allow=""
+    allowfullscreen
+  ></iframe>
+</template>
+
+<script setup>
+const props = defineProps({
+  videoId: {
+    type: [String, Number],
+    required: true,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+</style>
--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -1,13 +1,44 @@
 <template>
   <div>
-    <h2>Lesson</h2>
-    <p>This is a lesson</p>
-    <p>Chapter slug : {{ $route.params.chapterSlug }}</p>
-    <p>Lesson slug : {{ $route.params.lessonSlug }}</p>
+    <p>Lesson {{ chapter.number }} - {{ lesson.number }}</p>
+    <h2>{{ lesson.title }}</h2>
+
+    <div class="flex space-x-4 mt-2 mb-8">
+      <a
+        v-if="lesson.sourceUrl"
+        class="font-normal text-md text-gray-500"
+        :href="lesson.sourceUrl"
+      >
+        Download source code
+      </a>
+      <a
+        v-if="lesson.downloadUrl"
+        class="font-normal text-md text-gray-500"
+        :href="lesson.downloadUrl"
+      >
+        Download Video
+      </a>
+    </div>
+    <VideoPlayer v-if="lesson.videoId" :video-id="lesson.videoId" />
+    <p>{{ lesson.text }}</p>
   </div>
 </template>
 
 <script setup>
+const course = useCourse();
+const route = useRoute();
+
+const chapter = computed(() => {
+  return course.chapters.find(
+    (chapter) => chapter.slug == route.params.chapterSlug
+  );
+});
+
+const lesson = computed(() => {
+  return chapter.value.lessons.find(
+    (lesson) => lesson.slug === route.params.lessonSlug
+  );
+});
 </script>
 
 <style lang="scss" scoped>
